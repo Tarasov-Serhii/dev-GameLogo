068a384e9644181335dbf5f1a5b368a69be9e7a5
Serhii-Tarasov <tsa.21.1999@gmail.com>
2025-02-13 16:46:57 +0200

    news and porfilio pages

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,19 @@
-  
+const swiperDelivery = new Swiper('.swiper-custm', {
+    speed: 400,
+    loop: true,
+    simulateTouch: true,
+    slidesPerView: 3,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      enabled: true,
+    },
+    navigation: {
+      enabled: true,
+      nextEl: '.swiper-button-next-unique',
+      prevEl: '.swiper-button-prev-unique',
+    },
+
+    
+  }); 
